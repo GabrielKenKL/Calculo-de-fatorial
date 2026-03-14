7b41e5e6349837dc5add25803003671964f9382b
Gabriel Ken Kudo de Lima <gabkenkudo@gmail.com>
2026-03-14 19:19:44 -0300

    Primeira versão dos códigos.

---
+++ Calculo do fatorial de um numero.py
@@ -0,0 +1,7 @@
+#Receba um número inteiro. Calcule e mostre o seu fatorial.
+num = int(input("Digite um número inteiro: "))
+
+for i in range(1, num + 1):
+    fatorial *= i
+
+print("O fatorial de" , num , "é igual a" , fatorial)
